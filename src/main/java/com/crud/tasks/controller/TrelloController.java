a08e741404411faa6bf8c7b9ca9779c8b48f3e2d
Piotr Drzezdzon <jazzjohn78@gmail.com>
2023-05-20 13:14:38 +0200

    22.4 added tests

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -5,6 +5,7 @@ import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,35 +19,12 @@ public class TrelloController {
     private final TrelloClient trelloClient;
 
     @GetMapping("boards")
-    public void getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
-        trelloBoards.forEach(trelloBoardDto -> {
-            System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
-            System.out.println("This board contains lists:");
-            trelloBoardDto.getLists().forEach(trelloList -> {
-                System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
-            });
-        });
-
-        /*trelloBoards.stream()
-                .filter(b -> b.getName() != null)
-                .filter(b -> b.getId() != null)
-                .filter(b -> b.getName().contains("Kodilla"))
-                .map(b -> b.getId() + " " + b.getName())
-                .forEach(System.out::println);*/
+    public ResponseEntity<List<TrelloBoardDto>> getTrelloBoards() {
+        return ResponseEntity.ok(trelloClient.getTrelloBoards());
     }
 
     @PostMapping("cards")
-    public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
-        CreatedTrelloCard card = trelloClient.createNewCard(trelloCardDto);
-
-        System.out.println("Created card: " + card.getId() + " - " + card.getName() + " - " + card.getShortUrl());
-        System.out.println("Card has badges votes quantity: " + card.getBadges().getVotes());
-        System.out.println("Card's attachmentByType has Trello with board equal to: " + card.getBadges().getAttachments().getTrello().getBoard() +
-                " and card equal to: " + card.getBadges().getAttachments().getTrello().getCard());
-
-        return card;
+    public ResponseEntity<CreatedTrelloCard> createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+        return ResponseEntity.ok(trelloClient.createNewCard(trelloCardDto));
     }
 }
